1af42349248cc385c7a251c5c3e7e7ac5861e1f8
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-03-24 08:01:20 +0200

    fin

---
+++ js/main.js
@@ -0,0 +1,70 @@
+const modalForm = document.querySelector('.modal-form');
+const modalFormEmail = document.querySelector('#user-email');
+const modalFormUrl = document.querySelector('#user-url');
+const modalFormInputs = document.querySelectorAll('input');
+const modalFormBtn = document.querySelector('.modal-send-btn');
+
+const handleEmailvalidation = (e) => {
+  let isValid = false;
+  const emailRegex = new RegExp(
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  );
+  if (!emailRegex.test(modalFormEmail.value) && modalFormEmail.value !== '') {
+    e.target.classList.add('error');
+    e.target.nextElementSibling.classList.remove('hide');
+    isValid = false;
+  } else if (emailRegex.test(modalFormEmail.value)) {
+    e.target.classList.remove('error');
+    e.target.nextElementSibling.classList.add('hide');
+    isValid = true;
+  }
+  return isValid;
+};
+
+const handleUrlvalidation = (e) => {
+  let isValid = false;
+  const urlRegex = /^https:\/\//;
+  if (!urlRegex.test(modalFormUrl.value) && modalFormUrl.value) {
+    modalFormUrl.classList.add('error');
+    modalFormUrl.nextElementSibling.classList.remove('hide');
+    isValid = false;
+  } else if (urlRegex.test(modalFormUrl.value)) {
+    modalFormUrl.classList.remove('error');
+    modalFormUrl.nextElementSibling.classList.add('hide');
+    isValid = true;
+  }
+  return isValid;
+};
+
+const activateBtn = (e) => {
+  if (handleEmailvalidation(e) && handleUrlvalidation(e) && e.target.value) {
+    modalFormBtn.disabled = false;
+  } else {
+    modalFormBtn.disabled = true;
+  }
+};
+
+const handleSubmitForm = (e) => {
+  e.preventDefault();
+
+  const urlValue = modalFormUrl.value;
+
+  fetch(urlValue, {
+    method: 'POST',
+    body: new FormData(e.target.form),
+  })
+    .then((response) => {
+      if (response.ok) {
+        window.location.href = 'https://payproglobal.com';
+      } else {
+        throw new Error('Bad response');
+      }
+    })
+    .catch((error) => console.error('Error:', error));
+};
+
+modalFormInputs.forEach((el) => {
+  el.addEventListener('blur', activateBtn);
+});
+
+modalFormBtn.addEventListener('click', handleSubmitForm);
